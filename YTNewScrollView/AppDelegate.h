e4c098e461bbff59a1e1c30d02e5807ed5166f1e
TonyAng <13323650885@163.com>
2016-04-11 19:54:34 +0800

    YTNewScrollView
     新型循环滚动时图

---
+++ YTNewScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YTNewScrollView
+//
+//  Created by TonyAng on 16/4/11.
+//  Copyright © 2016年 TonyAng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
